ea9147f77c75f9d10a0a091338e6e30291d9569f
ravi.devarakonda1 <ravi.devarakonda@acrolinx.com>
2023-07-30 12:53:30 +0530

    Add evenodd java program.

---
+++ evenodd.java
@@ -0,0 +1,14 @@
+import java.util.scanner class Even or Odd
+{
+public static void main(String [] args){
+    scanner scn=new. scanner (System.in)
+    system.out.printIn("num:");
+    int n=sc.nextInt() ;
+    if (n%2==0){
+        system.out.println(n+"is a even num");
+    }
+    else{
+    system.out.println(n+"is a odd num");
+    }
+}
+}
